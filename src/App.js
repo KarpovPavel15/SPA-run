549223ad45ff3a4470615ea4c06db307acda2c3c
KPVBY <kpvbygm@gmail.com>
2020-09-29 14:37:57 +0300

    init: init project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        HEADER
+      </header>
+      <main>
+        Main
+      </main>
+      <footer>
+        Footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
